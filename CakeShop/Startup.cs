84990df4c83cb9d7184a88b04a0a3ec6300fe4b1
DESKTOP-LBBF962\bvale <barnavalentina98@yahoo.com>
2021-01-07 20:47:32 +0200

    pie model and repo category model and repo

--- CakeShop/Startup.cs
+++ CakeShop/Startup.cs
@@ -1,3 +1,4 @@
+using CakeShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,11 @@ namespace CakeShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IPieRepository>();
+            services.AddScoped<ICategoryRepository>();
+            services.AddControllersWithViews();
+          //  services.AddTransient();
+          //  services.AddSingleton();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
